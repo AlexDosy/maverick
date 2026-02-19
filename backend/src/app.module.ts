c31f8223adf6693039bc31549bd4c2c50b983a88
AlexDosy <66622992+AlexDosy@users.noreply.github.com>
2026-02-19 15:10:33 +0000

    RBAC module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from "./auth/auth.module";
 import { PrismaModule } from "./prisma/prisma.module";
+import { RbacModule } from "./rbac/rbac.module";
 
 @Module({
-  imports: [AuthModule, PrismaModule],
+  imports: [AuthModule, PrismaModule, RbacModule],
   controllers: [AppController],
   providers: [AppService]
 })
